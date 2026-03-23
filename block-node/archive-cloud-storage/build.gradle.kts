1c2250270e8a9b670fed04f1d77e0bba92eb0210
Ivan St. Ivanov <ivan.st.ivanov@gmail.com>
2026-03-23 11:38:18 +0200

    Rename module and remove unneeded dependencies
    Signed-off-by: Ivan St. Ivanov <ivan.st.ivanov@gmail.com>

--- block-node/archive-cloud-storage/build.gradle.kts
+++ block-node/archive-cloud-storage/build.gradle.kts
@@ -17,6 +17,4 @@ testModuleInfo {
     requires("org.assertj.core")
     requires("org.hiero.block.node.app.test.fixtures")
     runtimeOnly("org.junit.jupiter.params")
-    runtimeOnly("org.mockito")
-    runtimeOnly("org.mockito.junit.jupiter")
 }
